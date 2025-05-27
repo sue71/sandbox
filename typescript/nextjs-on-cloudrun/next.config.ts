d3c1a18e14bfd6d60d0afdbd30deb4f13c50a428
sue71 <s.masaki07@gmail.com>
2025-05-27 20:33:48 +0900

    chore: add cache header

--- typescript/nextjs-on-cloudrun/next.config.ts
+++ typescript/nextjs-on-cloudrun/next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=300, stale-while-revalidate=600",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
